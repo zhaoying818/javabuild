b54d0dccc84a68534ffdb6bcd830e384603e1dbd
zhaoying818 <zhaoying1258@qq.com>
2020-02-22 20:00:18 +0800

    init

---
+++ src/main/java/springbootdemo/User.java
@@ -0,0 +1,38 @@
+package springbootdemo;
+
+public class User {
+    private int id;
+    private String username;
+    private String password;
+
+    public User() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String toString() {
+        return "user=[id:" + id + ",username:" + username + ",password:" + password + "]";
+    }
+}
---
+++ src/main/java/springbootdemo/UserController.java
@@ -0,0 +1,36 @@
+package springbootdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * controller类
+ * @author Administrator
+ *
+ */
+@RestController
+@EnableAutoConfiguration
+@SpringBootApplication
+public class UserController {
+
+    @RequestMapping("/login")
+    public User login(){
+        User user= new User();
+        user.setId(123);
+        user.setUsername("qwe");
+        user.setPassword("456");
+        return user;
+    }
+
+    @RequestMapping("/print")
+    public String print1(){
+        return "HELLO WORLD";
+    }
+
+    public static void main(String[] args){
+        SpringApplication.run(UserController.class);
+    }
+}
